af70f6eeec72e0bc4fc9eade92344a7b643dd788
sliw39 <sliw39@gmail.com>
2023-06-24 12:45:50 +0200

    add typings

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,13 @@
 /* generates a pure js lib */
 import { defineConfig } from 'vite'
+import dts from 'vite-plugin-dts'
 
 export default defineConfig({
+    plugins: [
+        dts({
+          insertTypesEntry: true,
+        }),
+      ],
     build: {
         lib: {
             entry: 'src/hexmap.ts',
