0b26887aaa6a6d4ce003b106e70f2c2601ff2c90
sliw39 <sliw39@gmail.com>
2023-07-16 23:26:12 +0200

    rename hexmap => main

--- vite.config.ts
+++ vite.config.ts
@@ -4,7 +4,7 @@ import { defineConfig } from 'vite'
 export default defineConfig({
     build: {
         lib: {
-            entry: 'src/hexmap.ts',
+            entry: 'src/main.ts',
             name: 'HexMap',
             fileName: (format) => `hexmap.${format}.js`
         }
